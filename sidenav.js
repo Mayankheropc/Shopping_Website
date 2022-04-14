0bdf1d7868f76734f6ebe7e54eac3a8d11307872
Mayank Singh <67374046+Mayankheropc@users.noreply.github.com>
2022-04-14 18:16:21 +0530

    Add files via upload

---
+++ sidenav.js
@@ -0,0 +1,7 @@
+const open_sidenav = () => {
+    document.getElementById('sidenav').style.width = "25%";
+}
+
+const close_sidenav = () => {
+    document.getElementById('sidenav').style.width = "0%";
+}
